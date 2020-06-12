dafdf97415f29ea2ec4bf731f38b1a19d9072a7e
Joel Schwartz <Joel.Schwartz1@ibm.com>
2020-06-11 23:42:59 -0400

    Added inputting scores to the DB

--- GetScoresUsingSelenium/src/main/java/test1/NFLPredictor.java
+++ GetScoresUsingSelenium/src/main/java/test1/NFLPredictor.java
@@ -1,13 +1,10 @@
 package test1;
 
-import org.omg.Messaging.SYNC_WITH_TRANSPORT;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
 
 public class NFLPredictor {
 
@@ -19,27 +16,23 @@ public class NFLPredictor {
     static final String USER = "root";
     static final String PASS = "my-secret-pw";
 
-    public static ArrayList pickYourTeam() throws Exception {
-
-        Connection conn = null;
-        Statement stmt = null;
-        String teamName = null;
+    public static ArrayList getListOfTeamsFromSchedule() throws Exception {
         ArrayList<String> list = new ArrayList<String>();
 
         // Register JDBC driver
         Class.forName("com.mysql.jdbc.Driver");
 
         // Open a connection
-        conn = DriverManager.getConnection(DB_URL,USER,PASS);
+        Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
 
         // Execute a query
-        stmt = conn.createStatement();
+        Statement stmt = conn.createStatement();
 
         ResultSet rs = stmt.executeQuery("SELECT teamName FROM TestNFL.nflSchedule ORDER BY teamName ASC");
 
         // Extract data from result set
         while(rs.next()){
-            teamName = rs.getString("teamName");
+            String teamName = rs.getString("teamName");
             list.add(teamName);
         }
         // Clean-up environment
@@ -50,10 +43,8 @@ public class NFLPredictor {
         return list;
     }
 
-    public static ArrayList OpponentsGivenTeam(String team) throws Exception{
-        Connection conn = null;
-        Statement stmt = null;
-        String opponent = null;
+    public static ArrayList getOpponentGivenTeamName(String team) throws Exception{
+        ArrayList<String> list = new ArrayList<String>();
         ArrayList<String> week = new ArrayList<String>();
 
         week.add("weekOne");
@@ -74,23 +65,21 @@ public class NFLPredictor {
         week.add("weekSixteen");
         week.add("weekSeventeen");
 
-        ArrayList<String> list = new ArrayList<String>();
-
         for (int i=0; i<17; i++){
         // Register JDBC driver
         Class.forName("com.mysql.jdbc.Driver");
 
         // Open a connection
-        conn = DriverManager.getConnection(DB_URL,USER,PASS);
+        Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
 
             // Execute a query
-            stmt = conn.createStatement();
+            Statement stmt = conn.createStatement();
 
             ResultSet rs = stmt.executeQuery("SELECT teamName, " + week.get(i) + " from TestNFL.nflSchedule where teamName = '" + team + "'");
 
             // Extract data from result set
             while (rs.next()) {
-                opponent = rs.getString(week.get(i));
+                String opponent = rs.getString(week.get(i));
                 list.add(opponent);
             }
 
@@ -100,5 +89,46 @@ public class NFLPredictor {
             conn.close();
         }
         return list;
+    }
+
+    public static void insertScoreIntoDatabaseGivenTeamAndWeek(int weekNumberMinusOne, int predictedScore, int predictedOpponentScore, String team, String opponentTeam) throws Exception {
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> week = new ArrayList<String>();
+
+        week.add("weekOne");
+        week.add("weekTwo");
+        week.add("weekThree");
+        week.add("weekFour");
+        week.add("weekFive");
+        week.add("weekSix");
+        week.add("weekSeven");
+        week.add("weekEight");
+        week.add("weekNine");
+        week.add("weekTen");
+        week.add("weekEleven");
+        week.add("weekTwelve");
+        week.add("weekThirteen");
+        week.add("weekFourteen");
+        week.add("weekFifteen");
+        week.add("weekSixteen");
+        week.add("weekSeventeen");
+
+        // Register JDBC driver
+        Class.forName("com.mysql.jdbc.Driver");
+
+        // Open a connection
+        Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+        // Execute a query
+        Statement updateYourTeam = conn.createStatement();
+        updateYourTeam.executeUpdate("UPDATE TestNFL." + week.get(weekNumberMinusOne) + " SET predictedTeamScore = " + predictedScore + ", predictedOpponentScore = " + predictedOpponentScore + " WHERE teamName = '" + team + "'");
+
+        Statement updateOpponentTeam = conn.createStatement();
+        updateOpponentTeam.executeUpdate("UPDATE TestNFL." + week.get(weekNumberMinusOne) + " SET predictedTeamScore = " + predictedOpponentScore + ", predictedOpponentScore = " + predictedScore + " WHERE teamName = '" + opponentTeam + "'");
+
+        // Clean-up environment
+        updateYourTeam.close();
+        updateOpponentTeam.close();
+        conn.close();
     }
 }
--- GetScoresUsingSelenium/src/test/java/test1/nflPredictorTests.java
+++ GetScoresUsingSelenium/src/test/java/test1/nflPredictorTests.java
@@ -12,7 +12,7 @@ public class nflPredictorTests {
     public void selectYourTeams() throws Exception {
 
         NFLPredictor nflPredictor = new NFLPredictor();
-        List listOfNFLTeams = nflPredictor.pickYourTeam();
+        List listOfNFLTeams = nflPredictor.getListOfTeamsFromSchedule();
         JComboBox pickATeam = new JComboBox(listOfNFLTeams.toArray());
         Object[] message = {
                 "team", pickATeam,
@@ -21,7 +21,7 @@ public class nflPredictorTests {
 
         String whatTeam = pickATeam.getSelectedItem().toString();
 
-        List listOfOpponents = nflPredictor.OpponentsGivenTeam(whatTeam);
+        List listOfOpponents = nflPredictor.getOpponentGivenTeamName(whatTeam);
 
         for (int i=0; i<17; i++) {
             if (listOfOpponents.get(i).toString().equals("BYE")) {
@@ -36,16 +36,42 @@ public class nflPredictorTests {
                 panel.setLayout(new GridLayout(2, 2)); //2 rows, 4 columns
                 panel.add(new JLabel(whatTeam));
                 panel.add(new JLabel("          score :"));
-                JTextField textField = new JTextField(2);
-                panel.add(textField);
+                JTextField teamScoreInput = new JTextField(2);
+                teamScoreInput.setText("get text from db");
+                //TODO get score if already in DB ^
+                panel.add(teamScoreInput);
                 panel.add(new JLabel(listOfOpponents.get(i).toString()));
+                JTextField opponentScoreInput = new JTextField(2);
                 panel.add(new JLabel("          score:"));
-                JTextField textField2 = new JTextField(2);
-                panel.add(textField2);
+                opponentScoreInput.setText("get text from db op");
+                //TODO get score if already in DB ^
+                panel.add(opponentScoreInput);
 
                 int result = JOptionPane.showOptionDialog(null, panel, "Pick Your Winner",
-                        JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,
+                        JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE,
                         null, options1, null);
+
+                String opponentTeamName = listOfOpponents.get(i).toString().substring(2);
+
+                String teamScorePredictionString = teamScoreInput.getText();
+                int teamScorePrediction = Integer.parseInt(teamScorePredictionString);
+                String opponentScorePredictionString = opponentScoreInput.getText();
+                int opponentScorePrediction = Integer.parseInt(opponentScorePredictionString);
+//                System.out.println(teamScorePrediction);
+//                // TODO store your team score in DB
+//                System.out.println(opponentScorePrediction);
+//                // TODO store your opponent team score in DB
+
+                NFLPredictor.insertScoreIntoDatabaseGivenTeamAndWeek(i, teamScorePrediction, opponentScorePrediction, whatTeam, opponentTeamName);
+
+                //no = win, yes = lose
+                if (result == JOptionPane.NO_OPTION){
+                    System.out.println("yay win");
+                    // TODO enter into DB
+                }else if (result == JOptionPane.YES_OPTION){
+                    System.out.println("boo lose");
+                    // TODO enter into DB
+                }
             }
         }
     }
